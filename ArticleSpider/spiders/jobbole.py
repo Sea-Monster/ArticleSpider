f873859bb9f5ccd68df8cad5a5d0425d99047288
Sea-Monster <cristianofigo@126.com>
2017-09-27 15:52:33 +0800

    1 获取文章列表页中的文章url并交给scrapy下载后并进行解析 2 获取下一页的url并交给scrapy进行下载，下载完成后交给parse

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -2,24 +2,44 @@
 import scrapy
 import re
 from scrapy.http.response.html import HtmlResponse
+from scrapy.http import Request
+from urllib import parse
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
     # start_urls = ['http://blog.jobbole.com/']
-    start_urls = ['http://blog.jobbole.com/110287/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
         '''
+        1 获取文章列表页中的文章url并交给scrapy下载后并进行解析
+        2 获取下一页的url并交给scrapy进行下载，下载完成后交给parse
+        :type response: HtmlResponse
+        :param response:
+        :return:
+        '''
+        # 获取文章列表页中的文章url并交给scrapy下载后并进行解析
+        post_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
+        for post_url in post_urls:
+            # Request(url=post_url, callback=self.parse_detail)
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
 
+        # 提取下一页并交给scrapy进行下载
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse)
+
+    def parse_detail(self, response):
+        '''
+        提取文章的具体字段
         :type response: HtmlResponse
         :param response:
         :return:
         '''
 
         # 通过css选择器提取字段
-        a = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip()
         title = response.css(".entry-header h1::text").extract()
         create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
         praise_nums = response.css(".vote-post-up h10::text").extract()[0]
@@ -27,8 +47,20 @@ class JobboleSpider(scrapy.Spider):
         fav_nums = response.css(".bookmark-btn::text").extract()[0]
         match_re = re.match(".*?(\d+).*", fav_nums)
         if match_re:
-            fav_nums = match_re.group(1)
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract_first()
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
         content = response.css("div.entry").extract()[0]
-        tags = response.css("p.entry-meta-hide-on-mobile a::text").extract_first()
-    
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        tag_list = [element for element  in tag_list if not element.strip().endswith("评论")]
+        tags = ','.join(tag_list)
+
+        print("end")
+
 
