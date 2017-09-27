4452009c50aed6f5d0d9df9fdf70fdd4cd32faaa
Sea-Monster <cristianofigo@126.com>
2017-09-27 09:34:30 +0800

    使用main.py来调试Scrapy Spider

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 调试Spider用
+from scrapy.cmdline import execute
+import sys
+import os
+
+print(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# 启动Spider： 在命令行，scrapy crawl jobbole
+# JobboleSpider中的name：jobbole
+execute(['scrapy', 'crawl', 'jobbole'])
