0db88f370bc157ebd987faac82a5eef6dc3570c2
Sea-Monster <cristianofigo@126.com>
2017-09-29 15:42:00 +0800

    使用了Item loader替代原来的使用Item的方法

--- ArticleSpider/pipelines.py
+++ ArticleSpider/pipelines.py
@@ -6,7 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from scrapy.pipelines.images import ImagesPipeline
-import pymysql
+import pymysql  #mysqlclient装不上，只好用这个了
 from twisted.enterprise import adbapi
 
 class ArticlespiderPipeline(object):
@@ -59,18 +59,21 @@ class MysqlTwistedPipeline(object):
     def do_insert(self, cursor, item):
         # 执行具体插入
         insert_sql = """
-                    insert into jobbole_article(url_object_id,title,url,create_date,fav_nums)
-                    VALUES (%s, %s, %s, %s, %s)
+                    insert into jobbole_article(url_object_id,title,url,create_date,fav_nums,tags)
+                    VALUES (%s, %s, %s, %s, %s, %s)
                 """
         cursor.execute(insert_sql,
-                            (item['url_object_id'], item['title'], item['url'], item['create_date'], item['fav_nums']))
+                            (item['url_object_id'], item['title'], item['url'],
+                             item['create_date'], item['fav_nums'],
+                             item['tags']))
 
 
 class ArticleImagePipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
-        for ok,value in results:
-            image_file_path = value['path']
-        item['front_image_path'] = image_file_path  # 文件保存的实际位置, 以便以后保存到数据库或者其他地方
+        if 'front_image_path' in item:
+            for ok,value in results:
+                image_file_path = value['path']
+            item['front_image_path'] = image_file_path  # 文件保存的实际位置, 以便以后保存到数据库或者其他地方
         return item
 
         # return super().item_completed(results, item, info)
